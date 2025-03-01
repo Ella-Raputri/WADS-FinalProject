6fca47a7eec41fc50afcc73e0220d2593bcb1e5f
Ella R <ellaraputri@gmail.com>
2025-03-01 17:40:10 +0700

    feature: footer
    also dummy content in homepage to test

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,52 @@
+import { FaInstagram, FaYoutube, FaTiktok } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+
+const Footer = () => {
+  return (
+    <footer className="mt-10 font-poppins w-full red-navbar text-white py-4">
+      <div className="max-w-7xl mx-auto flex flex-col sm:flex-row items-center justify-between px-6">
+
+        <div className="text-center sm:text-left">
+          <p>© Copyright 2025 Group Raft & NMC</p>
+        </div>
+
+        <div className="flex space-x-4 mt-3 sm:mt-0">
+          <a
+            href="https://www.instagram.com/nmcbnmc/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-300"
+          >
+            <FaInstagram className="w-6 h-6" />
+          </a>
+          <a
+            href="mailto:nmc2024.bnmc@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-300"
+          >
+            <HiOutlineMail className="w-6 h-6" />
+          </a>
+          <a
+            href="https://www.youtube.com/@NMCBNMC"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-300"
+          >
+            <FaYoutube className="w-6 h-6" />
+          </a>
+          <a
+            href="https://www.tiktok.com/@nmcbnmc"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-300"
+          >
+            <FaTiktok className="w-5 h-5 mt-0.5" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
