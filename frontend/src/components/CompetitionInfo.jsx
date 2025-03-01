fca4a9e7f0d6d4a27e0f6ce72c284c6176b130d1
Rafael23082 <raf.anderson777@gmail.com>
2025-03-01 21:34:35 +0700

    Update Competitions Page

---
+++ frontend/src/components/CompetitionInfo.jsx
@@ -0,0 +1,14 @@
+export const CompetitionInfo = ({competition, isFirst}) => {
+    return(
+    <div className={`w-[90%] justify-self-center min-h-[50vh] h-auto flex flex-col gap-14 ${isFirst ? "mt-[7em]" : "mt-[4em]"} items-center md:flex-row lg:w-[80%] ${isFirst ? "md:mt-[4em]" : "md:mt-[2em]"}`}>    
+            <div className="w-[28%] min-w-70 bg-gray-400 rounded-[10px] h-[40vh] shrink-0 sm:min-w-60"></div>
+            <div className="w-[90%] flex flex-col justify-center h-auto md:w-[72%]">
+            <p className="font-semibold text-[1.2rem] font-kanit">{competition.title}</p>
+                <div className="text-[0.9rem] mt-1 text-[#818181] font-poppins">Category: {competition.category}</div>
+                <div className="text-[0.9rem] mt-1 text-[#000000] font-poppins">{competition.description}</div>
+                <div className="text-[0.9rem] mt-1 text-[#818181] font-poppins">Date: {competition.date}</div>
+                <button className="w-25 h-9 mt-3 color-component-red rounded-md hover:!bg-red-700 text-white">Register</button>
+            </div>
+        </div>
+    );
+}
--- frontend/src/pages/participant/CompetitionPage.jsx
+++ frontend/src/pages/participant/CompetitionPage.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { CompetitionInfo } from '../../components/CompetitionInfo';
+import { useState } from 'react';
 
-const CompetitionPage = () => {
-  return (
-    <div>CompetitionPage</div>
-  )
+const CompetitionPage = ({}) => {
+  const [competitions, setCompetitions] = useState([
+    {
+        title: "Traditional Chinese Songs Singing", 
+        category: "Singing", 
+        description: "This competition's theme revolves around singing Chinese traditional songs, where the committee will decide the songs for each contestant. The contestants will be given 30 minutes to prepare themselves, before recording them singing and submit it on the submission form provided.", 
+        date: "Dec 21 - 25"
+    },
+    {
+        title: "Traditional Chinese Songs Singing", 
+        category: "Singing", 
+        description: "This competition's theme revolves around singing Chinese traditional songs, where the committee will decide the songs for each contestant. The contestants will be given 30 minutes to prepare themselves, before recording them singing and submit it on the submission form provided.", 
+        date: "Dec 21 - 25"
+    }
+]);
+
+  return (  
+      <div className="pb-10">
+          {competitions.map((competition, index) => (
+            <CompetitionInfo competition={competition} key={index} isFirst={index===0} />
+      ))}
+      </div>
+  );
 }
 
-export default CompetitionPage
+
+export default CompetitionPage;
