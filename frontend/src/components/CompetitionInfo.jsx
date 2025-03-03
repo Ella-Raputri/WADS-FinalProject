7d4f07a80164e56d8a7a6d70685257063eeccd24
Rafael23082 <raf.anderson777@gmail.com>
2025-03-03 10:46:47 +0700

    UpdateHeight

--- frontend/src/components/CompetitionInfo.jsx
+++ frontend/src/components/CompetitionInfo.jsx
@@ -8,7 +8,7 @@ export const CompetitionInfo = ({competition, isFirst}) => {
     return(
     <>
     <div className={`w-[90%] justify-self-center min-h-[50vh] h-auto flex flex-col gap-14 ${isFirst ? "mt-[7em]" : "mt-[4em]"} items-center md:flex-row lg:w-[80%] ${isFirst ? "md:mt-[6em]" : "md:mt-[1em]"}`}>    
-            <div className="w-[28%] min-w-70 bg-gray-400 rounded-[10px] h-[40vh] shrink-0 sm:min-w-60"></div>
+            <div className="w-[28%] min-w-70 bg-gray-400 rounded-[10px] h-[40vh] shrink-0 sm:min-w-60 max-h-[250px]"></div>
             <div className="w-[90%] flex flex-col justify-center h-auto md:w-[72%]">
             <p className="font-semibold text-[1.2rem] font-kanit">{competition.title}</p>
                 <div className="text-[0.9rem] mt-1 text-[#818181] font-poppins">Category: {competition.category}</div>
