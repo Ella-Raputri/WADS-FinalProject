af8d41334564d754468f6bee634d050a5e96f0ae
ellisraputri <ellisraputri@gmail.com>
2025-03-06 00:29:28 +0700

    save button and search

---
+++ frontend/src/components/SaveButton.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function SaveButton({data}) {
+
+    const handleSaveList = () => {
+        if (data.length === 0) {
+          alert("No data available to save!");
+          return;
+        }
+      
+        const headers = Object.keys(data[0]);
+      
+        const csvContent = [
+          headers.join(","), 
+          ...data.map(row => headers.map(field => `"${row[field]}"`).join(",")) 
+        ].join("\n");
+      
+        const blob = new Blob([csvContent], { type: "text/csv" });
+        const url = URL.createObjectURL(blob);
+      
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = "tickets.csv";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+        URL.revokeObjectURL(url);
+      };
+      
+
+  return (
+    <>
+        <button className="font-semibold px-8 py-2 rounded-2xl bg-red-700 text-white hover:bg-red-800 hover:cursor-pointer"
+          onClick={handleSaveList}>
+            SAVE LIST
+          </button>
+    </>
+  )
+}
+
+export default SaveButton
