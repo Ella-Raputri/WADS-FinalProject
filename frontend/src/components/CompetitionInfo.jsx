7f90b012aea1c7acbbfdc246326b66ecde729d7a
Rafael23082 <raf.anderson777@gmail.com>
2025-03-05 14:47:55 +0700

    Res

--- frontend/src/components/CompetitionInfo.jsx
+++ frontend/src/components/CompetitionInfo.jsx
@@ -7,7 +7,7 @@ export const CompetitionInfo = ({competition, isFirst}) => {
     return(
     <>
     <div className={`w-[90%] justify-self-center flex flex-col gap-14 items-center md:flex-row lg:w-[80%] ${isFirst ? "mt-[8em]" : "md:mt-[4em] mt-[5em]"}`}>    
-        <div className="w-[28%] min-w-70 bg-gray-400 rounded-[10px] min-h-[250px] shrink-0 sm:min-w-60"></div>
+        <div className="w-[28%] bg-gray-400 rounded-[10px] min-h-[250px] shrink-0 min-w-[250px] sm:min-w-[300px]"></div>
         <div className="w-[90%] flex flex-col justify-center md:w-[72%]">
             <p className="font-semibold text-[1.2rem] font-kanit">{competition.title}</p>
             <div className="text-[0.9rem] mt-1 text-[#818181] font-poppins">Category: {competition.category}</div>
@@ -18,6 +18,7 @@ export const CompetitionInfo = ({competition, isFirst}) => {
             }}>Register</button>
         </div>
     </div>
+    
     { isOpen && 
     <CompetitionPopUp competition={competition} isRegistered={false} isOpen={isOpen} onClose={() => {
         setIsOpen(false);
