5f846c8e31044b74db98a6d656375c6e575a4560
Rafael23082 <raf.anderson777@gmail.com>
2025-03-05 17:38:32 +0700

    Res

--- frontend/src/components/CompetitionPopUp.jsx
+++ frontend/src/components/CompetitionPopUp.jsx
@@ -19,19 +19,19 @@ export const CompetitionPopUp = ({competition, isRegistered, isOpen, onClose}) =
     return (
         <Modal competition={competition} isOpen={isOpen} onRequestClose={onClose} className="font-poppins md:w-[80%] w-[90%] md:h-[80vh] h-[90vh] bg-white mx-auto shadow-xl relative overflow-y-auto scrollbar-thin rounded-[10px]" overlayClassName="flex justify-center items-center inset-0 fixed z-1000 bg-[rgba(0,0,0,0.5)] overflow-hidden">
             <div className="top-0 sticky pt-1 bg-white">
-                <p className="font-kanit text-[1.5rem] mt-[1.5em] ml-[2em] w-[65%] md:w-auto">{competition.title}</p>
-                <p className="ml-[4em] text-[0.8rem] mt-[1em]">Price: {competition.price} / Person</p>
-                <span className="text-[2.3rem] absolute top-0 right-0 mr-[1em] md:mr-[1.5em] mt-[0.4em] cursor-pointer" onClick={onClose}>&times;</span>
+                <p className="font-kanit text-[1.5rem] xl:text-[1.9rem] mt-[1.5em] ml-[2em] w-[65%] md:w-auto">{competition.title}</p>
+                <p className="ml-[4em] text-[0.8rem] xl:text-[1.2rem] mt-[1em]">Price: {competition.price} / Person</p>
+                <span className="text-[2.3rem] xl:text-[2.7rem] absolute top-0 right-0 mr-[1em] md:mr-[1.5em] mt-[0.4em] cursor-pointer" onClick={onClose}>&times;</span>
                 <div className="w-[80%] h-[0.05em] bg-gray-400 ml-[3.1em] mt-[0.5em]"></div>
             </div>
-            <p className="ml-[4em] text-[0.8rem] mt-[1em]">Venue: {competition.venue}</p>
-            <p className="ml-[4em] text-[0.8rem] mt-[0.6em]">Date: {competition.date}</p>
-            <p className="ml-[4em] text-[0.8rem] mt-[0.6em]">Time: {competition.time}</p>
-            <p className="ml-[4em] text-[0.8rem] mt-[0.6em]">Prizepool: {competition.prizepool}</p>
-            <p className="ml-[3.5em] text-[0.9rem] font-semibold mt-[1em]">Rules / Requirements</p>
+            <p className="ml-[4em] text-[0.8rem] xl:text-[1.2rem] mt-[1em]">Venue: {competition.venue}</p>
+            <p className="ml-[4em] text-[0.8rem] xl:text-[1.2rem] mt-[0.6em]">Date: {competition.date}</p>
+            <p className="ml-[4em] text-[0.8rem] xl:text-[1.2rem] mt-[0.6em]">Time: {competition.time}</p>
+            <p className="ml-[4em] text-[0.8rem] xl:text-[1.2rem] mt-[0.6em]">Prizepool: {competition.prizepool}</p>
+            <p className="ml-[3.5em] text-[0.9rem] xl:text-[1.3rem] font-semibold mt-[1em]">Rules / Requirements</p>
             <ul className="list-disc ml-[4em]">
                 {requirement_arr.map((line) => (
-                    <li className={`w-[80%] text-[0.8rem] mt-[0.6em]`}>{line}</li>
+                    <li className={`w-[80%] text-[0.8rem] xl:text-[1.2rem] mt-[0.6em]`}>{line}</li>
                 ))}
             </ul>
             <button className={`w-30 h-9 mt-5 ${isRegistered ? "bg-[#319340] hover:!bg-green-700" : "color-component-red hover:!bg-red-700"} rounded-md text-white text-center block mx-auto mb-[3em] cursor-pointer`}>{isRegistered ? "Registered" : "Register"}</button>
--- frontend/src/components/ScheduleList.jsx
+++ frontend/src/components/ScheduleList.jsx
@@ -29,18 +29,18 @@ export const SecheduleList = ({competition}) => {
 
     return (
         <>
-        <div className="flex w-[95%] lg:w-[90%] lg:min-h-[12vh] mx-auto mt-[2em] md:mt-[2em] sm:mt-[0.7em] mb-[1em] justify-center items-center lg:mt-[1em]">
+        <div className="flex w-[95%] lg:w-[90%] mx-auto mt-[2em] md:mt-[2em] sm:mt-[0.7em] mb-[1em] justify-center items-center lg:mt-[1em]">
             <div className="w-[20%] sm:flex-col sm:items-center sm:justify-center hidden sm:flex">
                 <p className="text-[0.7rem]">{day}</p>
                 <p className="font-medium text-[1.4rem]">{date}</p>
                 <p className="text-[0.7rem]">{month} {year}</p>
             </div>
-            <div className="w-[80%] bg-[#F4F4F4] rounded-[10px] text-[0.8rem] font-medium flex flex-col justify-center relative lg:min-h-[10em] lg:max-h-[200px] pt-4 pb-4 sm:pt-1 sm:pb-1 shadow">
+            <div className="w-[80%] bg-[#F4F4F4] rounded-[10px] text-[0.8rem] font-medium flex flex-col justify-center relative pt-4 pb-4 sm:pt-1 sm:pb-1 shadow">
                 <p className="ml-[2em] lg:w-[60%]">Competition Name: {competition.title}</p>
                 <p className="ml-[2em]">Category: {competition.category}</p>
                 <p className="ml-[2em]">Time: {competition.time}</p>
                 <p className={`ml-[2em]`}>Status: <p className={`inline-flex ${status === "Incoming" ? "text-[#DEC41B]" : "text-[#FF9000]"}`}>{status}</p></p>
-                <button className="lg:absolute lg:right-0 w-23 h-7 mt-3 lg:mr-10 ml-6 color-component-red rounded-md hover:!bg-red-700 text-white cursor-pointer" onClick={() => {
+                <button className="lg:absolute lg:right-0 w-23 h-7 mt-2 mb-2 lg:mb-0 lg:mr-10 ml-6 color-component-red rounded-md hover:!bg-red-700 text-white cursor-pointer" onClick={() => {
                     setIsOpen(true);
                 }}>Details</button>
             </div>
