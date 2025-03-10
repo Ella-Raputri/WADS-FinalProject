94dcf6d563d29ab45ab1ba3651a8fa7ce87a60f8
Ella R <ellaraputri@gmail.com>
2025-03-11 00:49:14 +0700

    feat: parallax animation 1st section welcome page

--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Carousel from '../components/Carousel';
 import Tassle from '../components/Tassle';
@@ -10,6 +10,46 @@ const WelcomePage = () => {
     navigate('/usercomp');
   };
 
+  const light1Ref = useRef(null);
+  const light2Ref = useRef(null);
+  const light3Ref = useRef(null);
+  const leftFlowerRef = useRef(null);
+  const rightFlowerRef = useRef(null);
+  const descRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let value = window.scrollY;
+
+      if (descRef.current) {
+        descRef.current.style.top = value * 2.5 + 'px';
+      }
+      if (leftFlowerRef.current) {
+        leftFlowerRef.current.style.top = value * -1.5 + 'px';
+        leftFlowerRef.current.style.left = value * 1.5 + 'px';
+      }
+      if (rightFlowerRef.current) {
+        rightFlowerRef.current.style.top = value * -1.5 + 'px';
+        rightFlowerRef.current.style.right = value * 1.5 + 'px';
+      }
+
+      const fadeValue = Math.max(0, 1 - value/200);
+      if(light1Ref.current){
+        light1Ref.current.style.opacity = fadeValue; 
+      }
+      if(light2Ref.current){
+        light2Ref.current.style.opacity = fadeValue; 
+      }
+      if(light3Ref.current){
+        light3Ref.current.style.opacity = fadeValue; 
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <>
@@ -19,67 +59,62 @@ const WelcomePage = () => {
         <img 
             src="src/assets/welcome_page/light3.png" 
             alt="Light3" 
-            id="light3"
-            className="absolute top-30 left-[50%] h-80 sm:left-[55%] md:h-100 xl:top-45 xl:h-120 xl:left-[58%]" 
+            ref={light3Ref}
+            className="absolute z-2 top-30 left-[50%] h-80 sm:left-[55%] md:h-100 xl:top-45 xl:h-120 xl:left-[58%]" 
           />
           <img 
             src="src/assets/welcome_page/light2.png" 
             alt="Light2" 
-            id='light2'
-            className="absolute top-30 left-[36%] h-80 sm:left-[40%] md:h-100 lg:left-[44%] xl:top-45 xl:h-120" 
+            ref={light2Ref}
+            className="absolute z-2 top-30 left-[36%] h-80 sm:left-[40%] md:h-100 lg:left-[44%] xl:top-45 xl:h-120" 
           />
           <img 
             src="src/assets/welcome_page/light1.png" 
             alt="Light1" 
-            id='light1'
-            className="absolute top-30 -left-[10%] h-80 sm:left-[8%] md:h-100 lg:left-[15%] xl:top-45 xl:h-120" 
+            ref={light1Ref}
+            className="absolute z-2 top-30 -left-[10%] h-80 sm:left-[8%] md:h-100 lg:left-[15%] xl:top-45 xl:h-120" 
           />
           <img
             src="src/assets/welcome_page/flower.png"
             alt="Flower Left"
-            id='flower_left'
+            ref={leftFlowerRef}
             className="absolute z-10 top-3 left-0 w-35 sm:w-50 md:w-60 lg:w-70 xl:w-80 2xl:w-90"
           />
           <img
             src="src/assets/welcome_page/flower2.png"
             alt="Flower Right"
-            id='flower_right'
-            className="absolute top-3 right-0 w-35 sm:w-50 md:w-60 lg:w-70 xl:w-80 2xl:w-90"
+            ref={rightFlowerRef}
+            className="absolute z-10 top-3 right-0 w-35 sm:w-50 md:w-60 lg:w-70 xl:w-80 2xl:w-90"
           />
           <img
             src="src/assets/welcome_page/tower.png"
             alt="Tower"
-            id='tower'
-            className="absolute top-70 left-30 w-14 sm:w-16 sm:top-60 md:w-18 lg:w-21 lg:left-35 lg:top-55 xl:w-25 xl:top-60 xl:left-42 2xl:w-30 2xl:top-65 2xl:left-45"
+            className="absolute z-10 top-70 left-30 w-14 sm:w-16 sm:top-60 md:w-18 lg:w-21 lg:left-35 lg:top-55 xl:w-25 xl:top-60 xl:left-42 2xl:w-30 2xl:top-65 2xl:left-45"
           />
           <img
             src="src/assets/welcome_page/temple.png"
             alt="Temple"
-            id='temple'
-            className="absolute top-82 -left-15 w-65 sm:w-70 sm:top-78 sm:-left-20 md:w-78 md:-left-22 md:top-80 lg:w-90 lg:-left-26 xl:w-110 xl:-left-30 xl:top-88 2xl:w-130 2xl:-left-35 2xl:top-95"
+            className="absolute z-10 top-82 -left-15 w-65 sm:w-70 sm:top-78 sm:-left-20 md:w-78 md:-left-22 md:top-80 lg:w-90 lg:-left-26 xl:w-110 xl:-left-30 xl:top-88 2xl:w-130 2xl:-left-35 2xl:top-95"
           />
           <img
             src="src/assets/welcome_page/ferris.png"
             alt="Ferris Wheel"
-            id='ferris'
-            className="absolute top-60 left-3/4 w-70 sm:w-73 sm:top-54 md:w-83 md:left-140 lg:left-3/4 lg:w-92 xl:w-110 2xl:w-130 2xl:top-50"
+            className="absolute z-10 top-60 left-3/4 w-70 sm:w-73 sm:top-54 md:w-83 md:left-140 lg:left-3/4 lg:w-92 xl:w-110 2xl:w-130 2xl:top-50"
           />
           <img
             src="src/assets/welcome_page/buildings.png"
             alt="Buildings"
-            id='buildings'
-            className="absolute top-83 left-3/5 w-130 sm:w-135 sm:top-80 md:w-160 md:top-81 md:left-105 lg:w-180 lg:left-1/3 xl:w-215 xl:left-5/12 xl:top-94 2xl:w-250 2xl:left-1/3 2xl:top-100 object-cover"
+            className="absolute z-10 top-83 left-3/5 w-130 sm:w-135 sm:top-80 md:w-160 md:top-81 md:left-105 lg:w-180 lg:left-1/3 xl:w-215 xl:left-5/12 xl:top-94 2xl:w-250 2xl:left-1/3 2xl:top-100 object-cover"
           />
           <img
             src="src/assets/welcome_page/palace.png"
             alt="Palace"
-            id='palace'
-            className="absolute top-98 left-30 w-82 sm:w-88 sm:top-94 md:w-110 md:top-95 md:left-20 lg:w-122 lg:left-30 lg:top-98 xl:w-145 xl:left-35 xl:top-113 2xl:w-170 2xl:left-40 2xl:top-120"
+            className="absolute z-10 top-98 left-30 w-82 sm:w-88 sm:top-94 md:w-110 md:top-95 md:left-20 lg:w-122 lg:left-30 lg:top-98 xl:w-145 xl:left-35 xl:top-113 2xl:w-170 2xl:left-40 2xl:top-120"
           />     
         </div>
 
         {/* Text & Button */}
-        <div className="relative z-15 mt-24 sm:mt-16">
+        <div className="relative z-5 mt-24 sm:mt-16" ref={descRef}>
           <h1 className="color-text-red font-extrabold text-6xl sm:text-7xl md:text-8xl xl:text-9xl 2xl:text-10xl font-raleway p-5 pb-3">
             NMC
           </h1>
