960ab22b1bac674cb95b9992c15694102db660ed
Rafael23082 <raf.anderson777@gmail.com>
2025-03-14 20:57:27 +0700

    Homepage Update

--- frontend/src/components/ScheduleList.jsx
+++ frontend/src/components/ScheduleList.jsx
@@ -15,14 +15,12 @@ export const SecheduleList = ({competition}) => {
         date = startDate.getDate();
         month = months[startDate.getMonth()];
         year = startDate.getFullYear();
-        status = "Incoming";
     }
     if (now > startDate && now < endDate){
         day = days[startDate.getDay()];
         date = startDate.getDate();
         month = months[startDate.getMonth()];
         year = startDate.getFullYear();
-        status = "Ongoing";
     }
 
     const [isOpen, setIsOpen] = useState(false);
@@ -35,14 +33,26 @@ export const SecheduleList = ({competition}) => {
                 <p className="font-medium text-[1.4rem] xl:text-[1.8rem]">{date}</p>
                 <p className="text-[0.7rem] xl:text-[1.1rem]">{month} {year}</p>
             </div>
-            <div className="w-[80%] bg-[#F4F4F4] rounded-[10px] text-[0.8rem] xl:text-[1.2rem] font-medium flex flex-col justify-center relative pt-4 pb-4 sm:pt-3 sm:pb-3 shadow">
-                <p className="ml-[2em] lg:w-[60%]">Competition Name: {competition.title}</p>
-                <p className="ml-[2em]">Category: {competition.category}</p>
-                <p className="ml-[2em]">Time: {competition.time}</p>
-                <p className={`ml-[2em]`}>Status: <p className={`inline-flex ${status === "Incoming" ? "text-[#DEC41B]" : "text-[#FF9000]"}`}>{status}</p></p>
-                <button className="lg:absolute lg:right-0 w-23 py-2 mt-3 lg:mb-0 lg:mr-10 ml-6 color-component-red rounded-md hover:!bg-red-700 text-white cursor-pointer" onClick={() => {
-                    setIsOpen(true);
-                }}>Details</button>
+            <div className="w-[80%] bg-[#F4F4F4] rounded-[10px] text-[0.8rem] xl:text-[1.2rem] font-medium flex flex-col justify-center sm:items-center sm:flex-row pt-4 pb-4 sm:pt-3 sm:pb-3 shadow">
+                <div className="pl-[2em] w-[90%] sm:w-[70%]">
+                    <p className="w-[100%] md:w-[80%]">Competition Name: {competition.title}</p>
+                    <p>Category: {competition.category}</p>
+                    <p>Time: {competition.time}</p>
+                    <button className="w-23 my-[0.7em] py-2 color-component-red rounded-md hover:!bg-red-700 duration-300 text-white cursor-pointer sm:hidden" onClick={() => {
+                        setIsOpen(true);
+                    }}>Details</button>
+                    <button className="w-23 py-2 ml-[1em] bg-blue-500 rounded-md hover:!bg-blue-700 duration-300 text-white cursor-pointer sm:hidden" onClick={() => {
+                        setIsOpen(true);
+                    }}>Status</button>
+                </div>
+                <div className="w-[50%] sm:w-[30%] hidden sm:flex sm:flex-col sm:justify-center sm:items-center gap-[10px] my-[0.7em]">
+                    <button className="w-23 py-2 color-component-red rounded-md hover:!bg-red-700 duration-300 text-white cursor-pointer" onClick={() => {
+                        setIsOpen(true);
+                    }}>Details</button>
+                    <button className="w-23 py-2 bg-blue-500 rounded-md hover:!bg-blue-700 duration-300 text-white cursor-pointer" onClick={() => {
+                        setIsOpen(true);
+                    }}>Status</button>
+                </div>
             </div>
         </div>
         { isOpen && 
