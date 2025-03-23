7dca0ecb6d0406d37f907ec4b2ee3f2727711c7c
Rafael23082 <raf.anderson777@gmail.com>
2025-03-23 13:49:30 +0700

    Homepage Update

--- frontend/src/components/StatusModal.jsx
+++ frontend/src/components/StatusModal.jsx
@@ -12,7 +12,7 @@ export const StatusModal = ({competition, isOpen, onClose}) => {
     }, [isOpen])
 
     return(
-        <Modal competition={competition} isOpen={isOpen} onRequestClose={onClose} className="items-center font-poppins flex flex-col w-[90%] md:w-[70%] bg-white mx-auto shadow-xl relative overflow-y-auto scrollbar-thin rounded-[10px]" overlayClassName="flex justify-center items-center inset-0 fixed z-1000 bg-[rgba(0,0,0,0.5)] overflow-hidden">
+        <Modal competition={competition} isOpen={isOpen} onRequestClose={onClose} className="items-center font-poppins flex flex-col w-[90%] md:w-[70%] bg-white mx-auto shadow-xl relative overflow-y-auto scrollbar-thin rounded-[10px] xl:max-w-[1200px] 2xl:max-w-[1800px]" overlayClassName="flex justify-center items-center inset-0 fixed z-1000 bg-[rgba(0,0,0,0.5)] overflow-hidden">
             <p className="font-kanit text-center text-[1.3rem] mt-[3em]">Competition Status</p>
             <span className="text-[2.3rem] xl:text-[2.7rem] absolute top-0 right-0 mr-[1em] md:mr-[1.5em] mt-[0.4em] cursor-pointer" onClick={onClose}>&times;</span>
             <div className="sm:grid sm:grid-cols-[auto_1fr] sm:gap-x-[30px] sm:gap-y-[1.5em] pl-[2em] pt-[2em] pb-[3em]">
