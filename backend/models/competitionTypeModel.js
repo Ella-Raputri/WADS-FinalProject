9af9bc818b6ac731bb704a2a54162e9ee43dde8e
Ella R <ellaraputri@gmail.com>
2025-04-02 19:23:26 +0700

    feat: seed comp type
    also fix some models

--- backend/models/competitionTypeModel.js
+++ backend/models/competitionTypeModel.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
+const CompetitionDateSchema = new mongoose.Schema({
+  StartDate: { type: Date, required: true },
+  EndDate: { type: Date, required: true },
+  FinalDate: { type: Date }
+});
+
 const CompetitionTypeSchema = new mongoose.Schema({
-    Name: { type: String, required: true },
-    Description: { type: String },
-    Price: { type: Number, required: true },
-    Venue: { type: String, required: true },
-    CompetitionDate: { type: Date, required: true },
-    CompetitionTime: { type: String, required: true },
-    MainPrize: { type: String },
-    GradingAspect: { type: String },
-  });
+  Name: { type: String, required: true },
+  Description: { type: [String], default: [] }, 
+  Price: { type: Number, required: true },
+  Venue: { type: String, required: true },
+  CompetitionDate: { type: CompetitionDateSchema, required: true },
+  MainPrize: { type: [String], default: [] }, 
+  GradingAspect: { type: [String], default: [] } 
+});
   
 const competitionTypeModel = mongoose.models.CompetitionType || mongoose.model('CompetitionType', CompetitionTypeSchema);
 
