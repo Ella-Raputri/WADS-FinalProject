24c861c28f4c75a68dd0be34be7ce046d7bd545c
ellisraputri <ellisraputri@gmail.com>
2025-04-29 17:34:00 +0700

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -78,8 +78,15 @@ export const register = async (req, res) => {
                 StudentCardPhoto: studentPhotoUrl
             },
         });
-
         await user.save();
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+        res.cookie('token', token,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production'? 'none':'strict',
+            maxAge: 7 *24 *60 *60 *1000
+        });
 
         return res.json({ success: true, message: "Account created successfully", user });
 
