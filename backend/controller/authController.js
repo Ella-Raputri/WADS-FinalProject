baead5190b6bca305e8d23cf62c6c3455fb67449
ellisraputri <ellisraputri@gmail.com>
2025-06-02 20:14:33 +0700

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -361,8 +361,8 @@ export const handleGoogleCallback = (req, res) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "None", // force explicit
-        domain: '.csbihub.id',
+        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'strict',
+        domain: process.env.NODE_ENV === 'production' ? '.csbihub.id' : undefined,
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
